b79ed2296d03672ec4cdd08e63bacfacd3b4d982
Bruxsa <vlada-cotic@mail.ru>
2017-06-10 14:58:29 +0300

    Create connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,23 @@
-<? 
+<?php
 $dbconfig = require('db_params.php');
-$mysqli = new mysqli($dbconfig['host'], $dbconfig['web_41'],
-$dbconfig['HLwtawuzsXK2zZJw'], $dbconfig['web_41']);
-$mysqli->set_charset('utf8');
+$mysqli = new mysqli($dbconfig['host'], $dbconfig['user'],
+$dbconfig['password'], $dbconfig['db']);
 if ($mysqli->connect_errno) {
  echo "Ошибка: Не удалсь создать соединение с базой MySQL и вот почему: \n";
  echo "Номер_ошибки: " . $mysqli->connect_errno . "\n";
  echo "Ошибка: " . $mysqli->connect_error . "\n";
  exit;
 }
+$sql = "SELECT * FROM records";
+if (!$result = $mysqli->query($sql)) {
+3
+ echo "Ошибка: Наш запрос не удался и вот почему: \n";
+ echo "Запрос: " . $sql . "\n";
+ echo "Номер_ошибки: " . $mysqli->errno . "\n";
+ echo "Ошибка: " . $mysqli->error . "\n";
+ exit;
+}
+$record = $result->fetch_assoc();
+echo $record['content'];
 ?>
 
