ce556de87da20308ec7bbe49bd309a2354628132
Bruxsa <vlada-cotic@mail.ru>
2017-06-10 15:26:04 +0300

    Create connection.php

--- connection.php
+++ connection.php
@@ -2,6 +2,7 @@
 $dbconfig = require('db_params.php');
 $mysqli = new mysqli($dbconfig['host'], $dbconfig['user'],
 $dbconfig['password'], $dbconfig['db']);
+$mysqli->set_charset('utf8');
 if ($mysqli->connect_errno) {
  echo "Ошибка: Не удалсь создать соединение с базой MySQL и вот почему: \n";
  echo "Номер_ошибки: " . $mysqli->connect_errno . "\n";
