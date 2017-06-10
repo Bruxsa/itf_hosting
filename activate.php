0b2430fad636bcd4da022ebac4cd1e9dd67288b3
Bruxsa <vlada-cotic@mail.ru>
2017-06-10 19:08:53 +0300

    Add files via upload

--- activate.php
+++ activate.php
@@ -1,22 +1,28 @@
 ﻿<?php
-include ("connection.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
+include ("connection.php");
 
+ if(!empty($_GET['approve_code']) && isset($_GET['approve_code']))
+ {
+ $approve_code=($_GET['approve_code']);
+ $c=mysqli_query($mysqli,"SELECT id FROM project WHERE approve_code='$approve_code'");
+ if(mysqli_num_rows($c) > 0)
+ {
+ $count=mysqli_query($mysqli,"SELECT project.id FROM project WHERE approve_code='$approve_code' and status_project='0'");
 
-$approve_code=$mysqli->query("SELECT project.approve_code FROM project")->fetch_assoc()->approve_code; 
+ if(mysqli_num_rows($count) == 1)
+ {
+mysqli_query($mysqli,"UPDATE project SET status_project='1' WHERE status_project=0 and approve_code='$approve_code'");
+ print "Заявка принята"; 
+ }
+ else
+ {
+ print "Заявка уже была принята";
+ }
+ }
+ else
+ {
+ print "Ошибка";
+ }
+ }
 
-
-if(isset($_GET['approve_code'])) {$approve_code =$_GET['approve_code']; } //код подтверждения 
-    else 
-		{ exit("Не указан генерированный код!");} //если не указали approve_code,    то выдаем ошибку
-	if(isset($_GET['approve_code'])) {$approve_code =$_GET['approve_code']; } //код подтверждения 
-    else 
-		{ exit("Не указан генерированный код!");} //если не указали approve_code,    то выдаем ошибку
-		if ($approve_code == $approve_code) {//сравниваем полученный из url и сгенерированный код 
-        $mysqli->query("UPDATE    project SET status_project='1' WHERE status_project=0 and approve_code='$approve_code'");//если равны, то активируем пользователя
-        print "Проект одобрен";
-    }
-    else {
-		print 'Ошибка: '; //если    же полученный из url и    сгенерированный код не равны, то выдаем ошибку
-         }
-	
 ?>
--- deactivate.php
+++ deactivate.php
@@ -1,22 +1,27 @@
 ﻿<?php
-include ("connection.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
+include ("connection.php");
 
+ if(!empty($_GET['reject_code']) && isset($_GET['reject_code']))
+ {
+ $reject_code=($_GET['reject_code']);
+ $c=mysqli_query($mysqli,"SELECT id FROM project WHERE reject_code='$reject_code'");
+ if(mysqli_num_rows($c) > 0)
+ {
+ $count=mysqli_query($mysqli,"SELECT project.id FROM project WHERE reject_code='$reject_code' and status_project='0'");
 
-$reject_code=$mysqli->query("SELECT project.reject_code FROM project")->fetch_assoc()->reject_code; 
-
-
-if(isset($_GET['reject_code'])) {$reject_code =$_GET['reject_code']; } //код подтверждения 
-    else 
-		{ exit("Не указан генерированный код!");} //если не указали reject_code,    то выдаем ошибку
-	if(isset($_GET['reject_code'])) {$reject_code =$_GET['reject_code']; } //код подтверждения 
-    else 
-		{ exit("Не указан генерированный код!");} //если не указали reject_code,    то выдаем ошибку
-		if ($reject_code == $reject_code) {//сравниваем полученный из url и сгенерированный код 
-        $mysqli->query("UPDATE    project SET status_project='2' WHERE status_project=0 and reject_code='$reject_code'");//если равны, то активируем пользователя
-        print "Проект одобрен";
-    }
-    else {
-		print 'Ошибка: '; //если    же полученный из url и    сгенерированный код не равны, то выдаем ошибку
-         }
-	
+ if(mysqli_num_rows($count) == 1)
+ {
+mysqli_query($mysqli,"UPDATE project SET status_project='2' WHERE status_project=0 and reject_code='$reject_code'");
+ print "Заявка отклонена"; 
+ }
+ else
+ {
+ print "Заявка уже была отклонена";
+ }
+ }
+ else
+ {
+ print "Ошибка";
+ }
+ }
 ?>
