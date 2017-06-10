67c03b578aca0e1d7a36c7b7a75415658c60a240
Bruxsa <vlada-cotic@mail.ru>
2017-06-10 14:35:02 +0300

    Create connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,13 @@
 <? 
-
-$mysqli = new mysqli('localhost','root','','host2');
+$dbconfig = require('db_params.php');
+$mysqli = new mysqli($dbconfig['host'], $dbconfig['web_41'],
+$dbconfig['HLwtawuzsXK2zZJw'], $dbconfig['web_41']);
 $mysqli->set_charset('utf8');
-if ($mysqli->connect_error) {
-  echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+if ($mysqli->connect_errno) {
+ echo "Ошибка: Не удалсь создать соединение с базой MySQL и вот почему: \n";
+ echo "Номер_ошибки: " . $mysqli->connect_errno . "\n";
+ echo "Ошибка: " . $mysqli->connect_error . "\n";
+ exit;
 }
 
 ?>
