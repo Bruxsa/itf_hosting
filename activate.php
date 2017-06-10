792656ac84ebef51923a646c759f45e8f43c7414
Bruxsa <vlada-cotic@mail.ru>
2017-06-10 18:03:31 +0300

    Add files via upload

--- activate.php
+++ activate.php
@@ -1,23 +1,22 @@
 ﻿<?php
-include "connection.php";
+include ("connection.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
+
 
 $approve_code=$mysqli->query("SELECT project.approve_code FROM project")->fetch_assoc()->approve_code; 
 
 
-	if(isset($_GET['approve_code'])) {
-		$approve_code =$_GET['approve_code']; //код подтверждения 
-		} else {
-			exit("Не указан генерированный код!");
-		} //если не указали approve_code,    то выдаем ошибку
-	if(isset($_GET['approve_code'])) {
-		$approve_code =$_GET['approve_code']; //код подтверждения 
-		} else { 
-			exit("Не указан генерированный код!");
-		} //если не указали approve_code,    то выдаем ошибку
-	if ($approve_code == $approve_code) {//сравниваем полученный из url и сгенерированный код 
-        $mysqli->query("UPDATE    project SET status_progect='1' WHERE status_progect=0 and approve_code='$approve_code'");//если равны, то активируем пользователя
-        echo "Проект одобрен <a    href='request.php'>Подача заявки</a>";
-		} else {echo "Ошибка! Проект не подтвержден!"; //если    же полученный из url и    сгенерированный код не равны, то выдаем ошибку
-        }
+if(isset($_GET['approve_code'])) {$approve_code =$_GET['approve_code']; } //код подтверждения 
+    else 
+		{ exit("Не указан генерированный код!");} //если не указали approve_code,    то выдаем ошибку
+	if(isset($_GET['approve_code'])) {$approve_code =$_GET['approve_code']; } //код подтверждения 
+    else 
+		{ exit("Не указан генерированный код!");} //если не указали approve_code,    то выдаем ошибку
+		if ($approve_code == $approve_code) {//сравниваем полученный из url и сгенерированный код 
+        $mysqli->query("UPDATE    project SET status_project='1' WHERE status_project=0 and approve_code='$approve_code'");//если равны, то активируем пользователя
+        print "Проект одобрен";
+    }
+    else {
+		print 'Ошибка: '; //если    же полученный из url и    сгенерированный код не равны, то выдаем ошибку
+         }
 	
 ?>
--- deactivate.php
+++ deactivate.php
@@ -1,24 +1,22 @@
 ﻿<?php
-include "connection.php";
-
-$reject_code=$mysqli->query("SELECT project.reject_code FROM project")->fetch_assoc()->reject_code;
+include ("connection.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
 
 
-	if(isset($_GET['reject_code'])) {
-		$reject_code =$_GET['reject_code']; //код подтверждения 
-	} else { 
-		exit("Не указан сгенерированный код!");
-	} //если не указали reject_code,    то выдаем ошибку
-	if(isset($_GET['reject_code'])) {
-		$reject_code =$_GET['reject_code']; //код подтверждения
-	} else { 
-		exit("Не указан сгенерированный код!");
-	} //если не указали reject_code,    то выдаем ошибку
-	if ($reject_code == $reject_code) {//сравниваем полученный из url и сгенерированный код 
-		$mysqli->query("UPDATE    project SET status_progect='2' WHERE status_progect=0 and reject_code='$reject_code'");//если равны, то активируем пользователя
-        print "Проект не одобрен <a    href='request.php'>Подача заявки</a>";
-    }else {
-		print 'Ошибка: ('. $mysqli->errno .') '. $mysqli->error ; //если    же полученный из url и    сгенерированный код не равны, то выдаем ошибку
+$reject_code=$mysqli->query("SELECT project.reject_code FROM project")->fetch_assoc()->reject_code; 
+
+
+if(isset($_GET['reject_code'])) {$reject_code =$_GET['reject_code']; } //код подтверждения 
+    else 
+		{ exit("Не указан генерированный код!");} //если не указали reject_code,    то выдаем ошибку
+	if(isset($_GET['reject_code'])) {$reject_code =$_GET['reject_code']; } //код подтверждения 
+    else 
+		{ exit("Не указан генерированный код!");} //если не указали reject_code,    то выдаем ошибку
+		if ($reject_code == $reject_code) {//сравниваем полученный из url и сгенерированный код 
+        $mysqli->query("UPDATE    project SET status_project='2' WHERE status_project=0 and reject_code='$reject_code'");//если равны, то активируем пользователя
+        print "Проект одобрен";
     }
+    else {
+		print 'Ошибка: '; //если    же полученный из url и    сгенерированный код не равны, то выдаем ошибку
+         }
 	
 ?>
