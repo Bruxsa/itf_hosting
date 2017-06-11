dfc2d3f897d0f87596e424baf6d5239ed066a97b
Bruxsa <vlada-cotic@mail.ru>
2017-06-11 17:17:57 +0300

    Update deactivate.php

--- deactivate.php
+++ deactivate.php
@@ -9,7 +9,7 @@ include ("connection.php");
  $count=mysqli_query($mysqli,"SELECT project.id FROM project WHERE reject_code='reject_code' and status_project='0'");
  if(mysqli_num_rows($count) == 1)
  {
-mysqli_query($mysqli,"UPDATE project SET status_project='2' WHERE status_project=0 and reject_code='reject_code'");
+mysqli_query($mysqli,"UPDATE project SET status_project='5' WHERE status_project=0 and reject_code='reject_code'");
  print "Заявка принята"; 
  }
  else
